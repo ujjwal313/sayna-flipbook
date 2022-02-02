ffbd170dd66be4f344c5e8c3da4994216d494156
ujjwal313 <ujjwalkr80@gmail.com>
2022-02-03 04:02:34 +0530

    update

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ $("#flipbook").turn({
   display: "single",
   width: 400,
   height: 400,
+  elevation: 80,
 });
 
 $("#first-btn").click(function () {
   $("#flipbook").turn("page", 1);
 });
+$("#prev-btn").click(function () {
+  $("#flipbook").turn("previous");
+});
+$("#next-btn").click(function () {
+  $("#flipbook").turn("next");
+});
+
+if ($(window).width() >= 768) {
+  $("#flipbook").turn("display", "double");
+}
