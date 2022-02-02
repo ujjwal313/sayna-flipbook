945c5cb118d53f43d35cf4e6a65568aec0e73a85
ujjwal313 <ujjwalkr80@gmail.com>
2022-02-02 23:50:02 +0530

    init

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-$("#flipbook").turn({
-  gradients: true,
-  acceleration: true,
-  autoCenter: true,
-  display: "single",
-  width: 500,
-  height: 400,
+// $("#flipbook").turn({
+//   gradients: true,
+//   acceleration: true,
+//   autoCenter: true,
+//   display: "single",
+//   width: 500,
+//   height: 400,
+// });
+
+// $("#first-btn").click(function () {
+//   $("#flipbook").turn("page", 1);
+// });
+
+var flipbookEL = document.getElementById("flipbook");
+
+window.addEventListener("resize", function (e) {
+  flipbookEL.style.width = "";
+  flipbookEL.style.height = "";
+  $(flipbookEL).turn("size", flipbookEL.clientWidth, flipbookEL.clientHeight);
 });
 
-$("#first-btn").click(function () {
-  $("#flipbook").turn("page", 1);
+$(flipbookEL).turn({
+  autoCenter: true,
+  gradients: true,
+  //   display: "single",
 });
